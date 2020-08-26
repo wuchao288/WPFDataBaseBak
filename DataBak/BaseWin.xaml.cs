b5a38c4ffd21115e2a6b280ccc0a8207c065a075
wuchao288 <371126627@qq.com>
2020-08-26 22:35:23 +0800

    ok

--- DataBak/BaseWin.xaml.cs
+++ DataBak/BaseWin.xaml.cs
@@ -24,5 +24,44 @@ namespace DataBak
             InitializeComponent();
             this.Style = (Style)App.Current.Resources["BaseWindowStyle"];
         }
+        Rect rcnormal;//
+        private void Button_Click_Max(object sender, RoutedEventArgs e)
+        {
+            Rect rc = SystemParameters.WorkArea;//获取工作区大小
+            rcnormal = new Rect(this.Left, this.Top, this.Width, this.Height);//保存下当前位置与大小
+            if (this.Width > 980)
+            {
+
+                this.Width = 980;
+                this.Height = 600;
+                this.Left = (rc.Width / 2) - (980 / 2);
+                this.Top = (rc.Height / 2) - (600 / 2);
+            }
+            else
+            {
+
+
+                this.Left = -14;//设置位置
+                this.Top = -14;
+
+                this.Width = rc.Width + 28;
+                this.Height = rc.Height + 28;
+            }
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void Button_Click_Min(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
     }
 }
